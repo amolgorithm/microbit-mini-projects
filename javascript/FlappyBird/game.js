0b05c2ca74338f5cba9299659781c4e9189a2dcb
Amol S <123842348+amolgorithm@users.noreply.github.com>
2023-06-30 12:42:37 -0400

    Update FlappyBird game.js

--- javascript/FlappyBird/game.js
+++ javascript/FlappyBird/game.js
@@ -8,49 +8,82 @@ const birdX: number = 1;
 let pipeX: number = 5;
 let pipeYPoints = [1, 0, 4];
 
+// Game states
 let pauseDown: boolean = false;
 let gameOver: boolean = false;
+let gameStarted: boolean = false;
 
-let score = 0;
+let score = 0; //score
+
+
+// Resets global variables to initial value
+function resetVariables() {
+    birdY = 1;
+
+    pipeX = 5;
+    pipeYPoints = [1, 0, 4];
+
+    pauseDown = false;
+    gameOver = false;
+
+    score = 0;
+}
 
 
 basic.forever(function on_forever() {
-    basic.clearScreen();
+    if (gameStarted) {
+        basic.clearScreen();
 
-    if (gameOver) {
-        game.pause();
-        game.setScore(score);
-        game.gameOver();
+        if (gameOver) {
+            game.pause();
+            game.setScore(score);
+            game.gameOver();
+        }
+
+        led.plotBrightness(1, birdY, 255);
+        
+        for (let i = 0; i < pipeYPoints.length; i++) {
+            led.plotBrightness(pipeX, pipeYPoints[i], 100);
+        }
+
+        pipeCollisionCheck();
+    } else {
+        basic.clearScreen();
+        basic.showString("Flappy Bird! Press Button A to play.");
     }
-
-    led.plotBrightness(1, birdY, 255);
-    
-    for (let i = 0; i < pipeYPoints.length; i++) {
-        led.plotBrightness(pipeX, pipeYPoints[i], 100);
-    }
-
-    pipeCollisionCheck();
 });
 
 loops.everyInterval(500, function() {
-    if (birdY >= 4) gameOver = true;
-    if (!pauseDown) birdY++;
+    if (gameStarted) {
+        if (birdY >= 4) gameOver = true;
+        if (!pauseDown) birdY++;
 
-    if (pipeX < 0) {
-        generateRandomPipePoints();
-        pipeX = 5;
-        score++;
+        if (pipeX < 0) {
+            generateRandomPipePoints();
+            pipeX = 5;
+            score++;
+        }
+        pipeX--;
     }
-    pipeX--;
+});
+
+
+input.onButtonPressed(Button.A, function() {
+    resetVariables();
+    led.stopAnimation();
+    gameStarted = !gameStarted;
 });
 
 input.onButtonPressed(Button.B, function() {
-    birdY--;
-    pauseDown = true;
-    basic.pause(250);
-    birdY--;
-    pauseDown = false;
+    if (gameStarted) {
+        birdY--;
+        pauseDown = true;
+        basic.pause(250);
+        birdY--;
+        pauseDown = false;
+    }
 });
+
 
 function pipeCollisionCheck() {
     for (let i = 0; i < pipeYPoints.length; i++) {
@@ -64,7 +97,7 @@ function generateRandomPipePoints() {
     let emptyPoint1 = randint(1, 3);
     let emptyPoint2 = randint(1, 3);
 
-    while (emptyPoint2 == emptyPoint1) {
+    while (Math.abs(emptyPoint2 - emptyPoint1) != 1) {
         emptyPoint2 = randint(1, 3);
     }
 
